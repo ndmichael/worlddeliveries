8bbcd190dd615f5648f54b76c6855ef331363042
ndmichael <ukejemicheal@gmail.com>
2022-10-04 10:47:06 +0100

    added del url

--- client/urls.py
+++ client/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import dashboard, create_shipment, receipt
+from .views import dashboard, create_shipment, receipt, del_package
 
 
 
 urlpatterns = [
     path('create-shipment/', create_shipment, name="create-shipment" ),
     path('package/receipt/<int:id>', receipt, name="shipment-receipt" ),
+    path('package/<int:id>/delete/', del_package, name="package-delete" ),
     path('<str:username>/', dashboard, name="client-dashboard" ),
 ]
